fc210a4b907978ed987264808a8b010f6333e8be
Botmasher <languagejosh@hotmail.com>
2016-05-05 15:15:48 -0400

    Build grid and identify game spaces within grid

---
+++ GameGrid.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameGrid : MonoBehaviour {
+
+	// reference to move space prefab
+	public GameObject moveSpace;
+
+	// grid dimensions
+	public int rowCount;
+	public int columnCount;
+
+	// grid origins 
+	public float xOrigin;
+	public float yOrigin;
+
+	// store all instantiated spaces
+	private List<GameObject> grid;
+
+	/*
+	// player sides with entry points for each player
+	private int numberOfPlayers;
+	public GameObject entrySpace;
+	*/
+
+
+	void Start () {
+
+		// empty list for spawned game spaces
+		grid = new List<GameObject> ();
+		// current locations for placing spawn objects within grid
+		float xLoc = xOrigin;
+		float yLoc = yOrigin;
+
+		// build out the grid row by row bottom to top
+		for (int i = 0; i < rowCount; i++) {
+			// reset column origin each row
+			xLoc = xOrigin;
+			for (int j = 0; j < columnCount; j++) {
+				// spawn one game space at this x,y and parent to grid
+				GameObject space = Instantiate (moveSpace, new Vector3 (xLoc, yLoc, 0f), Quaternion.identity) as GameObject;
+				space.transform.parent = this.transform;
+				// store column and row as within game space as identifier
+				space.GetComponent<GameSpace> ().rowIndex = i;
+				space.GetComponent<GameSpace> ().columnIndex = j;
+				// add game space to list
+				grid.Add (space);
+				// move to new grid locations
+				xLoc++;
+			}
+			yLoc ++;
+		}
+	}
+	
+
+	void Update () {
+	
+	}
+}
--- GameSpace.cs
+++ GameSpace.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class GameSpace : MonoBehaviour {
 
+	// toggle color lerp from other scripts
 	public bool changingColors;
+
+	// the color to change to
 	public Color targetColor;
+
+	// ids to check row and column location from other scripts
+	public int rowIndex;
+	public int columnIndex;
 
 
 	void Update () {
