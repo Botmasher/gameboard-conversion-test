9e1732ad7f4dcd227693a2e6265e47b5dae1dfa3
Botmasher <languagejosh@hotmail.com>
2016-05-09 17:43:10 -0400

    Add Player class script to scripts folder

---
+++ Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// player color
+	private Color color;
+	public Color tokenColor {
+		get {
+			return color;
+		} set {
+			color = value;
+		}
+	}
+
+}
